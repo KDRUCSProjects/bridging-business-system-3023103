21f6819be1f233f3f2daeb2ce956769eafd1bb87
safiullah <safiullahjalalzai119@gmail.com>
2023-05-15 21:02:07 +0430

    feat(BusinessOwner,Address,Product): BusinessOwner,Address and Product are added

--- src/services/api/admin.py
+++ src/services/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Address, Product, BusinessOwner
 
 # Register your models here.
+
+admin.site.register(Address)
+admin.site.register(BusinessOwner)
+admin.site.register(Product)
--- src/services/api/models.py
+++ src/services/api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class BusinessOwner(models.Model):
+    name = models.CharField(max_length=60)
+    bio = models.TextField(blank=True, null=True)
+    phone = PhoneNumberField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    province = models.CharField(max_length=60)
+    district = models.CharField(max_length=60)
+    area = models.CharField(max_length=60)
+    street = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.province + "-" + self.district
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    # business=models.ForeignKey(Business,on_delete=models.CASCADE)
+    # category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
