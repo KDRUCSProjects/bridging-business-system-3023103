55a7c73330dc2e0ae773044725918e7123b16b35
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2023-05-15 11:52:41 +0430

    FolderStructure+Config+theme
    added FolderStructure+Config+theme

--- src/presentation/web/src/App.js
+++ src/presentation/web/src/App.js
@@ -1,13 +1,29 @@
-import Hello from '@components/Hello'
-function App() {
+// components
+import ScrollToTop from './components/ScrollToTop';
+import { ProgressBarStyle } from './components/ProgressBar';
+import NotistackProvider from './components/NotistackProvider';
+import MotionLazyContainer from './components/animate/MotionLazyContainer';
+import ThemeSettings from './components/settings';
+import Router from './routes';
+// theme
+import ThemeProvider from './theme';
+
+
+// ----------------------------------------------------------------------
+
+export default function App() {
   return (
-    <>
-    <Hello/>
-    <div className="App">
-      React is Ready for Developmentasdfasd
-    </div>
-    </>
+    <MotionLazyContainer>
+      <ThemeProvider>
+        <ThemeSettings>
+          <NotistackProvider>
+            <ProgressBarStyle />
+          
+            <ScrollToTop />
+            <Router />
+          </NotistackProvider>
+        </ThemeSettings>
+      </ThemeProvider>
+    </MotionLazyContainer>
   );
 }
-
-export default App;
