b7d229810a82923bbd2284e3f08450dfbfb16146
khalid ahmad <khalidahmadghiasi@gmail.com>
2023-05-17 10:08:54 +0430

    Models BusinessFavoriteProduct,ContactUs,Message and Ratting  added

--- src/services/api/admin.py
+++ src/services/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Address, Product, BusinessOwner,ProductColor,ProductImage,Category
+from .models import Address, Product, BusinessOwner,ProductColor,ProductImage,Category,Ratting,BusinessFavoriteProduct,Message,ContactUs
 
 # Register your models here.
 
@@ -9,3 +9,7 @@ admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(ProductColor)
 admin.site.register(Category)
+admin.site.register(Ratting)
+admin.site.register(BusinessFavoriteProduct)
+admin.site.register(Message)
+admin.site.register(ContactUs)
