518da72d344b4be4cf29a253c5ee5925f792485c
safiullah <safiullahjalalzai119@gmail.com>
2023-05-19 13:26:42 +0430

    feat(admins): Models admins configured

--- src/services/api/admin.py
+++ src/services/api/admin.py
@@ -18,9 +18,46 @@ from .models import (
 
 
 # Register your models here.
+
+
+class BusinessOwnerAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone", "email"]
+
+    class Meta:
+        model = BusinessOwner
+
+
+class BussinessAdmin(admin.ModelAdmin):
+    list_display = ["name", "business_owner", "business_type", "email", "phone"]
+
+    class Meta:
+        model = Business
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ["province", "district", "area", "street"]
+
+    class Meta:
+        model = Address
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+    class Meta:
+        model = Category
+
+
 class ProductImageAdmin(admin.StackedInline):
     model = ProductImage
     extra = 1
+
+
+class ProductColorAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+    class Meta:
+        model = ProductColor
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -31,28 +68,59 @@ class ProductAdmin(admin.ModelAdmin):
         model = Product
 
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["sender", "recever", "created_at"]
+
+    class Meta:
+        model = Message
+
+
+class BusinessFavoriteProductAdmin(admin.ModelAdmin):
+    list_display = ["business", "product"]
+
+    class Meta:
+        model = BusinessFavoriteProduct
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ["business", "create_at", "text"]
+
+    class Meta:
+        model = ContactUs
+
+
+class RattingAdmin(admin.ModelAdmin):
+    list_display = ["business", "product", "ratting_stars"]
+
+    class Meta:
+        model = Ratting
+
+
+class PaymentAdmin(admin.StackedInline):
+    model = Payment
+    extra = 1
+
+
 class OrderDetialAdmin(admin.StackedInline):
     model = OrderDetail
     extra = 1
 
 
 class OrderAdmin(admin.ModelAdmin):
-    inlines = [OrderDetialAdmin]
+    inlines = [OrderDetialAdmin, PaymentAdmin]
 
     class Meta:
         model = Order
 
 
-admin.site.register(Address)
-admin.site.register(BusinessOwner)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(BusinessOwner, BusinessOwnerAdmin)
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductImage)
-admin.site.register(ProductColor)
-admin.site.register(Category)
-admin.site.register(Ratting)
+admin.site.register(ProductColor, ProductColorAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Ratting, RattingAdmin)
 admin.site.register(BusinessFavoriteProduct)
-admin.site.register(Message)
-admin.site.register(ContactUs)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Business)
-admin.site.register(Payment)
+admin.site.register(Business, BussinessAdmin)
