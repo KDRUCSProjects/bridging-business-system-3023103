d20023cd7c108d6e8a344dc0a2a843c34e162797
safiullah <safiullahjalalzai119@gmail.com>
2023-05-19 14:48:23 +0430

    feat(view & serializer): view and  serializer of product and productImage are added

---
+++ src/services/api/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from .views import *
+from django.urls import include, path
+
+router = routers.DefaultRouter()
+
+router.register(r"product", PrdocutViewSet)
+router.register(r"product_image", ProductImageViewSet)
+
+urlpatterns = [path("", include(router.urls))]
--- src/services/api/views.py
+++ src/services/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Product, ProductImage
+from .serializers import ProductSerializer, ProductImageSerializer
 
 # Create your views here.
+
+
+class PrdocutViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductImageViewSet(viewsets.ModelViewSet):
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
