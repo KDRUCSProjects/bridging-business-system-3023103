48c9ac09a9819284e3a12f63c36fa2faef15039c
safiullah <safiullahjalalzai119@gmail.com>
2023-05-20 10:00:03 +0430

    feat(Order & OrderDetail): serializer, view and path for order and order Detail are added

--- src/services/api/urls.py
+++ src/services/api/urls.py
@@ -6,5 +6,7 @@ router = routers.DefaultRouter()
 
 router.register(r"product", PrdocutViewSet)
 router.register(r"product_image", ProductImageViewSet)
+router.register(r"order", OrderViewSet)
+router.register(r"order_detial", OrderDetailViewSet)
 
 urlpatterns = [path("", include(router.urls))]
