7323a0c444365ded48614981b26c6b42a62fc72d
saboorhemat <saboorhemat4600@gmail.com>
2023-05-24 09:49:30 +0430

    rout changed

--- src/presentation/web/src/routes/paths.js
+++ src/presentation/web/src/routes/paths.js
@@ -14,7 +14,7 @@ const PATH_PAGE_ROOT = '/';
 export const PATH_AUTH = {
   login: path(ROOTS_AUTH, '/login'),
   register: path(ROOTS_AUTH, '/register'),
-  adproduct: path(ROOTS_AUTH, '/adproduct'),
+  adproduct: path(ROOTS_AUTH, '/ad/product'),
 };
 
 // Root Pages
