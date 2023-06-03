f90cf111ca942d1e4cef279ac071c28c638415a9
saboorhemat <saboorhemat4600@gmail.com>
2023-06-03 22:39:25 +0430

    validation added

--- src/presentation/web/src/locale/Pashto/resetPassword.js
+++ src/presentation/web/src/locale/Pashto/resetPassword.js
@@ -1,7 +1,7 @@
 
 
 const resetPassword = {
-    'Forgot your password?': `کور پاڼه `,
+    'Forgot your password?': `خپل پاسورډ مو له یاده وتلی؟ `,
   'Please enter the email address associated with your account and We will email you a link to reset your password.': `مهربانی وکړی د خپل حساب سره تړلی بریښنالیک ادرس دننه کړی او موږ به ستاسو بریښنالیک ته یو لینک درکړو ستاسو د پټ نوم دبیا تنظمولو لپاره `,
     'Email address': `بریښنا لیک `,
     'send request': `غوښتنه لیږل  `,
