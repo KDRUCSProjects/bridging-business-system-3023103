39e6f495c8f0862867c1be7ae12186599b47cc23
Mohammad Haroon <103740969+Haroon500@users.noreply.github.com>
2023-06-04 15:13:26 +0430

    productdetiallocalaization

---
+++ src/presentation/web/src/locale/Pashto/CardDetails.js
@@ -0,0 +1,17 @@
+const CardDetails = {
+    'Product Details': `د محصول توضیحات `,
+    Home: `کور `,
+    AVAILABLE: `د لاسرسی وړ `,
+    Color: `رنګ  `,
+    'reviews':'پینځه لیدونکی ',
+    Size: ' اندازه   ',
+    'Size Chart':'اندازه ',
+
+    Quantity:'تعداد ',
+    Available:'د لاسرسی وړ ',
+    'Add to Cart':'کارټ ته یی اضافه کړۍ',
+    ' View Profile': 'پروفایل وګوري ',
+    discription:'توضیحات',
+  };
+  
+  export default CardDetails;
