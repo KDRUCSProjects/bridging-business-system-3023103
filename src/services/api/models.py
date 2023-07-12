42a337eabfb2fbc41ab879f9ebea6be7c572ab88
safiullah <safiullahjalalzai119@gmail.com>
2023-07-12 15:13:58 +0430

    fix(user model): errors are solved while user want to login and manager error

--- src/services/api/models.py
+++ src/services/api/models.py
@@ -10,25 +10,18 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 import uuid
-from .manager import UserManager
 
 
 class User(AbstractUser):
-    username = None
     email = models.EmailField(unique=True)
     is_verified = models.BooleanField(default=False)
     otp = models.CharField(max_length=6, null=True, blank=True)
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    objects = UserManager()
 
     def name(self):
         return self.first_name + " " + self.last_name
 
     def __str__(self):
-        return self.email
+        return self.username
 
 
 class BusinessOwner(models.Model):
@@ -53,6 +46,7 @@ class Address(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- src/services/backend/custom_authentication.py
+++ src/services/backend/custom_authentication.py
@@ -1,24 +1,24 @@
-# from django.contrib.auth import get_user_model
-# from django.contrib.auth.backends import BaseBackend
-# from django.contrib.auth.models import User
-# from django.db.models import Q
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import BaseBackend
+from django.db.models import Q
 
 
-# class CustomAuthenticationBackend(BaseBackend):
-#     def get_user(self, user_id):
-#         try:
-#             return User.objects.get(pk=user_id)
-#         except User.DoesNotExist:
-#             return None
+class CustomAuthenticationBackend(BaseBackend):
+    def get_user(self, user_id):
+        user = get_user_model()
+        try:
+            return user.objects.get(pk=user_id)
+        except user.DoesNotExist:
+            return None
 
-#     def authenticate(self, request, username=None, password=None, *kwargs):
-#         user_model = get_user_model()
-#         try:
-#             user = user_model.objects.get(
-#                 Q(username__exact=username) | Q(email__exact=username)
-#             )
-#             if user.check_password(password):
-#                 return user
+    def authenticate(self, request, username=None, password=None, *kwargs):
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(
+                Q(username__exact=username) | Q(email__exact=username)
+            )
+            if user.check_password(password):
+                return user
 
-#         except user_model.DoesNotExist:
-#             return None
+        except user_model.DoesNotExist:
+            return None
