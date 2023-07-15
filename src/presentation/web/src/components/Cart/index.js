0ba22e3b741a04f498ff3c8574e1743a3e785c1d
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2023-07-15 20:42:40 +0430

    Front/ShopCart/Badge
    added Front/ShopCart/Badge

--- src/presentation/web/src/components/Cart/index.js
+++ src/presentation/web/src/components/Cart/index.js
@@ -1,18 +1,18 @@
 import { alpha, styled } from '@mui/material/styles';
-import { Tooltip } from '@mui/material';
+import { Tooltip, Badge, Box } from '@mui/material';
 import { ShoppingCartRounded } from '@mui/icons-material';
 import cssStyles from '../../utils/cssStyles';
 import { IconButtonAnimate } from '../animate';
 
 const RootStyle = styled('span')(({ theme }) => ({
   ...cssStyles(theme).bgBlur({ opacity: 0.64 }),
-  right: 0,
+  right: '1%',
   top: '35%',
   position: 'fixed',
   marginTop: theme.spacing(-3),
   padding: theme.spacing(0.5),
   zIndex: theme.zIndex.drawer + 2,
-  borderRadius: '24px 0 20px 24px',
+  borderRadius: '50%',
   boxShadow: `-12px 12px 32px -4px ${alpha(
     theme.palette.mode === 'light' ? theme.palette.grey[600] : theme.palette.common.black,
     0.36
@@ -29,12 +29,16 @@ const Cart = () => {
             p: 1.25,
             transition: (theme) => theme.transitions.create('all'),
             '&:hover': {
-              color: 'primary.main',
+              color: 'secondary.main',
               bgcolor: (theme) => alpha(theme.palette.primary.main, theme.palette.action.hoverOpacity),
             },
           }}
         >
-          <ShoppingCartRounded />
+          <Badge badgeContent="30" anchorOrigin={{ color: 'error.main', vertical: 'top', horizontal: 'left' }}>
+            <Box sx={{ padding: '3px' }}>
+              <ShoppingCartRounded sx={{ color: 'primary.main' }} />
+            </Box>
+          </Badge>
         </IconButtonAnimate>
       </Tooltip>
     </RootStyle>
