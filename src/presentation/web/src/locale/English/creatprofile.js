42365e36fde25b562a229dcd88c9d28608c8f97e
khalid ahmad <khalidahmadghiasi@gmail.com>
2023-07-17 09:10:11 +0430

    Createprofile  locale in english added

--- src/presentation/web/src/locale/English/creatprofile.js
+++ src/presentation/web/src/locale/English/creatprofile.js
@@ -1,16 +1,16 @@
 const creatprofile = {
-  'Full Name' : `پوره نوم`,
-  'Phone Number': `د موبایل شمیره `,
-  'State/Region': `ایالت/سیمه`,
-  Address: `پته `, 
-  Company: ' شرکت  ',
- 'Email Address':'بریښنا لیک',
- Country:'مملکت',
- City: 'ښار',
- 'Zip/Code' :'د زیپ کوډ ',
- Role: 'رول ',
-'create user': 'ثیت کول ',
-'uplad image': 'انڅور پورته کړئ',
+  'Full Name' : `Full Name`,
+  'Phone Number': `Phone Number `,
+  'State/Region': `State/Region`,
+  Address: `Address`, 
+  Company: ' Company',
+ 'Email Address':'Email Address',
+ Country:'Country',
+ City: 'City',
+ 'Zip/Code' :'Zip/Code ',
+ Role: 'Role',
+'create user': 'Create User ',
+'uplad image': 'Upload Image',
 
 
   };
