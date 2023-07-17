bdb223e10a575ee7e3471386dd3b5c289ec71628
khalid ahmad <khalidahmadghiasi@gmail.com>
2023-07-17 09:15:28 +0430

    resetpassword  locale in english added

--- src/presentation/web/src/locale/English/resetPassword.js
+++ src/presentation/web/src/locale/English/resetPassword.js
@@ -1,11 +1,11 @@
 
 
 const resetPassword = {
-    'Forgot your password?': `خپل پاسورډ مو له یاده وتلی؟ `,
-  'Please enter the email address associated with your account and We will email you a link to reset your password.': `مهربانی وکړی د خپل حساب سره تړلی بریښنالیک ادرس دننه کړی او موږ به ستاسو بریښنالیک ته یو لینک درکړو ستاسو د پټ نوم دبیا تنظمولو لپاره `,
-    'Email address': `بریښنا لیک `,
-    'send request': `غوښتنه لیږل  `,
-    Back: `شاته تګ `,
+    'Forgot your password?': `Forgot your password?`,
+  'Please enter the email address associated with your account and We will email you a link to reset your password.': `Please enter the email address associated with your account , We will email you a link to reset your password.`,
+    'Email address': `Email Address`,
+    'send request': `Send Request`,
+    Back: `Back`,
     
   };
   
