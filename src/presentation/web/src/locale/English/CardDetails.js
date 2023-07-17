7d7cf8e4b1462c617542ab401bd06fe1663ca2c6
khalid ahmad <khalidahmadghiasi@gmail.com>
2023-07-17 09:03:44 +0430

    CardDetails locale in english added

---
+++ src/presentation/web/src/locale/English/CardDetails.js
@@ -0,0 +1,16 @@
+const CardDetails = {
+    'Product Details': `Product Details`,
+    AVAILABLE: `Available`,
+    Color: `Color`,
+    'reviews':'Reviews',
+    Size: 'Size',
+    'Size Chart':'Size Chart',
+
+    Quantity:'Quantity',
+    Available:'Available',
+    'Add to Cart':'Add to Cart',
+    ' View Profile': 'View Profile',
+    discription:'Discription',
+  };
+  
+  export default CardDetails;
