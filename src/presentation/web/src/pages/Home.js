45b63f74ed7e0c60f51b3494df5daaef9f8480c1
Wahab Adil <115516734+Wahab-Adil@users.noreply.github.com>
2023-07-18 21:32:47 +0430

    Front/DynamicSnack
    Added Front/DynamicSnack

--- src/presentation/web/src/pages/Home.js
+++ src/presentation/web/src/pages/Home.js
@@ -1,5 +1,13 @@
 // @mui
+import React from 'react';
+import Lottie from 'react-lottie';
+import { Button, Box } from '@mui/material';
 import { styled, useTheme } from '@mui/material/styles';
+import animationSetter from '../animations/animationSetter';
+
+
+import animation from '../animations/shop/cart (2).json';
+
 // components
 import Page from '../components/Page';
 
@@ -14,6 +22,7 @@ import useResponsive from '../hooks/useResponsive';
 import CustomSlider from '../components/CustomSlider';
 import Cart from '../components/Cart';
 import ImageSlider from '../components/ImageSlider';
+import Snack from '../components/Snack';
 
 // Card
 import ShopProductList from '../sections/shop/ShopProductList';
@@ -37,6 +46,22 @@ export default function HomePage() {
   const theme = useTheme();
   const { translate } = useLocales();
   const isMatchMobile = useResponsive('down', 'sm');
+
+  const [snackOptions, setSnackOptions] = React.useState({
+    open: true,
+    vertical: 'top',
+    horizontal: 'center',
+    backgroundColor:theme.palette.primary.main,
+    color:theme.palette.text.primary,
+    animation:<Lottie options={animationSetter(animation)} width='12em' height='4em' />,
+    message:'yes this is Dynamic One !',
+    animationPosition:{marginLeft:"-4em"}
+  });
+
+  const handleSnackClose = () => {
+    setSnackOptions({ ...snackOptions, open: false });
+  };
+
   return (
     <Page title="Ecommerce Start Here">
       <ContentStyle>
@@ -64,6 +89,18 @@ export default function HomePage() {
           />
         )}
         <Cart />
+        <Snack
+        vertical={snackOptions.vertical}
+        horizontal={snackOptions.horizontal}
+        open={snackOptions.open}
+        onClose={handleSnackClose}
+          message={snackOptions.message}
+          animation={snackOptions.animation}
+          autoHideDuration={5000}
+          backgroundColor={snackOptions.backgroundColor}
+          color={snackOptions.color}
+          animationPosition={snackOptions.animationPosition}
+        />
         <ShopProductList />
       </ContentStyle>
     </Page>
